b77ccba36c099c57cc2d4cf7b95d91cbab57bf52
Thchoonlophon <qianchenjin192518@gmail.com>
2020-03-18 15:51:38 +0800

    Merge all the.bat files

--- todo.bat
+++ todo.bat
@@ -1,7 +1,10 @@
 @echo off
 set pythonPath="F:\envs\py37\Scripts\python.exe">nul
 set mainPath="F:\py\todo\main.py">nul
-set param0=%0
+set param0=%0>nul
+set opera=%1>nul
+set id=%2>nul
+set content>nul
 cls
-%pythonPath% %mainPath% %param0%
+%pythonPath% %mainPath% %param0% %opera% %id% %content%
 echo on
