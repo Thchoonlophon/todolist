a081c56f53eb96f236585691141c5ab78dc02bba
thchoonlophon <qianchenjin192518@gmail.com>
2020-03-19 15:59:49 +0800

    add something I forgot before

--- todo.bat
+++ todo.bat
@@ -4,7 +4,7 @@ set mainPath="F:\py\todo\main.py">nul
 set param0=%0>nul
 set opera=%1>nul
 set id=%2>nul
-set content>nul
+set content=%3>nul
 cls
 %pythonPath% %mainPath% %param0% %opera% %id% %content%
 echo on
